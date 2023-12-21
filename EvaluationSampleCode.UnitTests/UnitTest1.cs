f3ef2cba4166b83dd42a7d18e2bf6d09973e8a5d
thomixlz <thomasbregler@protonmail.com>
2023-12-21 16:02:54 +0100

    Ajoutez des fichiers projet.

---
+++ EvaluationSampleCode.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EvaluationSampleCode.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

c63105fb4562b16f561943a9001f6657ea9ae5c0
thomixlz <thomasbregler@protonmail.com>
2023-12-21 16:06:23 +0100

    add reservation test

---
+++ EvaluationSampleCode.UnitTests/ReservationTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluationSampleCode.UnitsTests
+{
+    [TestClass]
+    public class ReservationTest
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            // Arrange
+            var adminUser = new User { IsAdmin = true };
+            var reservation = new Reservation(new User());
+
+            // Act
+            var result = reservation.CanBeCancelledBy(adminUser);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_SameUserCancelling_ReturnsTrue()
+        {
+            // Arrange
+            var user = new User();
+            var reservation = new Reservation(user);
+
+            // Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_AnotherUserCancelling_ReturnsFalse()
+        {
+            // Arrange
+            var user = new User();
+            var anotherUser = new User();
+            var reservation = new Reservation(user);
+
+            // Act
+            var result = reservation.CanBeCancelledBy(anotherUser);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
